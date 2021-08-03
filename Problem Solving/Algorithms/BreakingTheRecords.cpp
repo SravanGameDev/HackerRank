036f4ef68954136f5c39520653bb7675536c7a37
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-08-03 22:23:04 +0530

    Created project

---
+++ Problem Solving/Algorithms/BreakingTheRecords.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> breakingRecords(vector<int> scores)
+{
+    vector<int> record;
+    int min = 05;
+    int max = 04;
+
+    int maxScore = 0, minScore = 0;
+
+    for (int i = 0; i <unsigned(scores.size()); i++)
+    {
+        int value = 0;
+        value = scores[i];
+        if (value >= maxScore)
+        {
+            maxScore = value;
+        }
+    }
+    
+    record.push_back(min);
+    record.push_back(max);
+
+    return record;
+}
+
+int main()
+{
+    vector<int> scores{ 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
+
+    vector<int> record=breakingRecords(scores);
+
+    for (size_t i = 0; i < record.size(); i++)
+    {
+        cout << record[i] << " ";
+    }
+
+    cin.get();
+}
